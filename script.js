53dcccd4054ed7facaebf1169eff9b6e62ef2ff9
Amanda Marques <amandamarqueslinger@gmail.com>
2026-04-23 09:44:11 -0300

    criação de verbas e ajustes de layout

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById("footer").innerHTML = `
   <p>© 2026 - Guias Sonner</p>
 `;
+
+
+
+const btn = document.getElementById("toggleBtn");
+const menu = document.getElementById("menuLateral");
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("ativo");
+});
